76820a11e7eafc7411a04df3f5745a6580ca1791
JocelynTse <jocelyn.s.tse@gmail.com>
2018-08-22 20:22:58 -0500

    Added validation for various fields, incl unique username requirement

--- project-2-group-4/models/users.js
+++ project-2-group-4/models/users.js
@@ -1,17 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
     var users = sequelize.define("users", {
+      indexes: [
+        {
+          unique: true,
+          fields: ['uname']
+        }
+      ],
       uname: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-
+          len: [5, 25]
         }
       },
       pw: {
         type: DataTypes.TEXT,
         allowNull: false,
         validate: {
-
+          len: [8, 50]
         }
       }
     });
